b979c5af54d439e07045d13718e748c78ffaf076
Thiago Lacerda <thiaguinhoLrodrigues007@gmail.com>
2023-05-16 23:14:34 -0300

    organizado

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import Dashboard from "../../components/Home/Dashboard/Dashboard";
-import Footer from "../../components/Home/Footer/Footer";
 import Header from "../../components/Home/Header/Header";
 import Hero from "../../components/Home/Hero/Hero";
 import Partners from "../../components/Home/Partners/Partners";
+import Dashboard from "../../components/Home/Dashboard/Dashboard";
+import Footer from "../../components/Home/Footer/Footer";
 
 function Home() {
   return (
